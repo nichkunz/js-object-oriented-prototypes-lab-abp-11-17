7e1f9f5da740bee1dbb505e0d3e6800ed9ed14c8
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-12 20:34:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function BoardMember(name, homeState, training){
     return 'You can do that!'
   }
   BoardMember.prototype.doCharity = function() {
-    return 'I like to help people'
+    return 'I like to help people.'
   }
   BoardMember.prototype.releasePressStatement = function() {
     return 'You will see great things from Scuber.'
