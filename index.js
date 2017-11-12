86408456d88e89bd49c50d6ab5db697edd7a300f
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-12 20:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ function BoardMember(name, homeState, training){
   BoardMember.prototype.approve = function() {
     return 'You can do that!'
   }
-  
-  this.doCharity = function(){
-    return "I like to help people."
+  BoardMember.prototype.doCharity = function() {
+    return 'I like to help people'
   }
+  BoardMember.prototype.releasePressStatement = function() {
+    return 'You can do that!'
+  }
+  
 
   this.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
-  // 
+  //
   // this.sayHi = function(){
   //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   // }
